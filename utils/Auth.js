18b9fd3f74fdd16076f5e521b34efc3d95307908
SaboSuke <abedissam95@gmail.com>
2020-12-30 21:14:28 +0100

    auth

--- utils/Auth.js
+++ utils/Auth.js
@@ -47,55 +47,52 @@ const userRegister = async (userDets, role, res) => {
  * @DESC To Login the user (ADMIN, SUPER_ADMIN, USER)
  */
 const userLogin = async(userCreds, role, res) => {
-    let { email, password } = userCreds;
-    // First Check if the username is in the database
-    const user = await User.findOne({ email });
-    if (!user) {
-        return res.status(404).json({
-            message: "Email is not found. Invalid login credentials.",
-            success: false
-        });
-    }
-    // We will check the role
-    if (user.role !== role) {
-        return res.status(403).json({
-            message: "Please make sure you are logging in from the right portal.",
-            success: false
-        });
-    }
-    // That means user is existing and trying to signin fro the right portal
-    // Now check for the password
-    let isMatch = await bcrypt.compare(password, user.password);
-    if (isMatch) {
-        // Sign in the token and issue it to the user
-        let token = jwt.sign({
-                user_id: user._id,
-                role: user.role,
-                username: user.username,
-                email: user.email
-            },
-            SECRET, { expiresIn: "7 days" }
-        );
+  let { email, password } = userCreds;
+  const user = await User.findOne({ email });
+  if (!user) {
+    return res.status(404).json({
+      message: "Email is not found. Invalid login credentials.",
+      success: false,
+    });
+  }
+  if (user.role !== role) {
+    return res.status(403).json({
+      message: "Please make sure you are logging in from the right portal.",
+      success: false,
+    });
+  }
+  let isMatch = await bcrypt.compare(password, user.password);
+  if (isMatch) {
+    // Sign in the token and issue it to the user
+    let token = jwt.sign(
+      {
+        user_id: user._id,
+        role: user.role,
+        username: user.username,
+        email: user.email,
+      },
+      SECRET,
+      { expiresIn: "7 days" }
+    );
 
-        let result = {
-            username: user.username,
-            role: user.role,
-            email: user.email,
-            token: `Bearer ${token}`,
-            expiresIn: 168
-        };
-
-        return res.status(200).json({
-            ...result,
-            message: "Hurray! You are now logged in.",
-            success: true
-        });
-    } else {
-        return res.status(403).json({
-            message: "Incorrect password.",
-            success: false
-        });
-    }
+    let result = {
+      username: user.username,
+      role: user.role,
+      email: user.email,
+      token: `Bearer ${token}`,
+      expiresIn: 168,
+    };
+    return res.status(200).json({
+      ...result,
+      message: "Hurray! You are now logged in.",
+      success: true,
+    });
+  } else {
+    return res.status(403).json({
+      message: "Incorrect password.",
+      success: false,
+    });
+  }
 };
 
 /**
